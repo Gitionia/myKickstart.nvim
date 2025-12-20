f50445864a845b4fd8274a95bf549ccc0b2d511b
Gitionia <ben.boettcher@online.de>
2025-12-20 23:28:24 +0100

    split up init.lua into multiple .lua files

---
+++ lua/keymaps.lua
@@ -0,0 +1,34 @@
+-- [ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+local map = vim.keymap.set
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Note: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- map("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- map("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- map("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- map("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+--
+
+map('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggles the file window (nvim-tree)' })
